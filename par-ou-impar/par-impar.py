6740822a6657a9debad261be917c134cbde99e26
Gabriel12Vieira04 <bielsobrenatural99@gmail.com>
2024-05-06 20:20:55 -0400

    intro

--- par-ou-impar/par-impar.py
+++ par-ou-impar/par-impar.py
@@ -1,3 +1,11 @@
 # Menssagen(em qual numero vc pensou)
 #Entrada: 45
 #Saída: Esse é um número ímpar! Gostaria de verificar outro
+
+
+num = int(input("Em qual número você pensou?"))
+
+if num % 2 == 0:
+    print("Esse é um número par!")
+else:
+    print("É um número impar!")
